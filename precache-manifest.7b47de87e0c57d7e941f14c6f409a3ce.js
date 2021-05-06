59052d40b06a63cf3aaacb554f485394e658ba08
Femi <ologunwafemi@gmail.com>
2021-05-06 21:22:49 +0100

    Updates

---
+++ precache-manifest.7b47de87e0c57d7e941f14c6f409a3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fffd2fa374e3a7bb9398cf13a13c88",
+    "url": "/8-reactjs-lorem-text-generator-project/index.html"
+  },
+  {
+    "revision": "a79602842a32af01bfd8",
+    "url": "/8-reactjs-lorem-text-generator-project/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/8-reactjs-lorem-text-generator-project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/8-reactjs-lorem-text-generator-project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79602842a32af01bfd8",
+    "url": "/8-reactjs-lorem-text-generator-project/static/js/main.428283b0.chunk.js"
+  },
+  {
+    "revision": "ff8c1725313f02e832b7",
+    "url": "/8-reactjs-lorem-text-generator-project/static/js/runtime-main.4f021a30.js"
+  }
+]);
